c13c729879a4aa87c10ef466b7e5aa82706b298a
mahithsc <mahith.chitrapu@gmail.com>
2022-02-23 21:44:52 -0500

    making a crappy modal

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Post = () => {
+
+
+
     return (
         <div className='bg-slate-200 h-28 mx-8 rounded mt-7'>
         </div>
--- pages/modal.tsx
+++ pages/modal.tsx
@@ -1,4 +1,7 @@
+import { TextField } from '@mui/material'
+import Link from 'next/link'
 import React, { useState } from 'react'
+import Button from '@mui/material/Button';
 
 const modal = () => {
 
@@ -6,9 +9,22 @@ const modal = () => {
 
     return (
         <div className='flex justify-center items-center h-screen'>
-            <div className='bg-blue-500 h-96 w-96 rounded-xl'>
+            <div className='bg-blue-500 h-96 w-96 rounded-xl relative'>
                 <h1 className='text-white justify-center flex text-2xl'>Post</h1>
-                
+                <TextField
+                    id="outlined-basic"
+                    variant="outlined"
+                    value={post}
+                    onChange={(e) => {
+                        setPost(e.target.value)
+                    }}
+                    className='flex mt-6'
+                />
+                <div className='flex justify-center absolute bottom-5 left-1/2'>
+                    <Link href='/'>
+                        <Button className='text-black flex justify-center' variant="text">Post</Button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
