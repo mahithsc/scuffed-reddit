558a460d081ee530c81604861c1c4e077c5e6de6
mahithsc <mahith.chitrapu@gmail.com>
2022-02-23 23:14:30 -0500

    adds functions which add and remove upvotes

--- firebase/firebaseFunctions.tsx
+++ firebase/firebaseFunctions.tsx
@@ -1,13 +1,28 @@
 import { db } from './firebase'
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { addDoc, collection, serverTimestamp, updateDoc, doc, getDoc, increment } from 'firebase/firestore'
 import { stripVTControlCharacters } from 'util'
 
+//creates a post
 const createPost = async (title: string) => {
-        await addDoc(collection(db, 'posts'), {
-            title: title,
-            votes: 0,
-            timeStamp: serverTimestamp()
-        })
+    await addDoc(collection(db, 'posts'), {
+        title: title,
+        votes: 0,
+        timeStamp: serverTimestamp()
+    })
 }
 
-export { createPost }
+//adds vote to the post
+const addUpvote = async (id: string) => {
+    await updateDoc(doc(db, 'posts', id), {
+        votes: increment(1)
+    })
+}
+
+//decrease votes from the post
+const decreaseVote = async (id: string) => {
+    await updateDoc(doc(db, 'posts', id), {
+        votes: increment(-1)
+    })
+}
+
+export { createPost, addUpvote, decreaseVote }
