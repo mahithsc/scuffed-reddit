e98e4dbed0daf4ba655efe481296d25c48ad8551
mahithsc <mahith.chitrapu@gmail.com>
2022-02-23 22:45:41 -0500

    adding the functionality of creating posts

--- firebase/firebaseFunctions.tsx
+++ firebase/firebaseFunctions.tsx
@@ -1,2 +1,13 @@
-import {db} from './firebase'
+import { db } from './firebase'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { stripVTControlCharacters } from 'util'
 
+const createPost = async (title: string) => {
+        await addDoc(collection(db, 'posts'), {
+            title: title,
+            votes: 0,
+            timeStamp: serverTimestamp()
+        })
+}
+
+export { createPost }
--- pages/modal.tsx
+++ pages/modal.tsx
@@ -2,10 +2,11 @@ import { TextField } from '@mui/material'
 import Link from 'next/link'
 import React, { useState } from 'react'
 import Button from '@mui/material/Button';
+import { createPost } from '../firebase/firebaseFunctions';
 
 const modal = () => {
 
-    const [post, setPost] = useState<String>("")
+    const [post, setPost] = useState<string>("")
 
     return (
         <div className='flex justify-center items-center h-screen'>
@@ -22,7 +23,12 @@ const modal = () => {
                 />
                 <div className='flex justify-center absolute bottom-5 left-1/2'>
                     <Link href='/'>
-                        <Button className='text-black flex justify-center' variant="text">Post</Button>
+                        <Button
+                            className='text-black flex justify-center' 
+                            variant="text" 
+                            onClick={() => {createPost(post)}}>
+                            Post
+                        </Button>
                     </Link>
                 </div>
             </div>
