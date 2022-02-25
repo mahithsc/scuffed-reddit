7730fcaaa8c99c30bf2a2cd7598d2af74994978f
mahithsc <mahith.chitrapu@gmail.com>
2022-02-25 18:06:45 -0500

    bringing all args and setting up state variables

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import {} from '../firebase/firebase'
+import {} from 'firebase/firestore'
 
 const Post = ({ text, votes, id }) => {
+
+    const [docid, setId] = useState(id)
+    const [upvotes, setUpvotes] = useState(votes)
+
+
+    useEffect(() => {
+        
+    }, [])
+
     return (
         <div className='bg-slate-200 h-28 mx-8 rounded mt-7 relative flex items-center'>
             <div className='flex flex-col items-center h-full justify-center absolute left-5'>
@@ -11,7 +24,7 @@ const Post = ({ text, votes, id }) => {
                 </button>
 
 
-                <div>{votes}</div>
+                <div>{upvotes}</div>
 
                 <button>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
