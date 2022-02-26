ffc1b9135eec9abb3d4b06453be31f2ac1073c7d
mahithsc <mahith.chitrapu@gmail.com>
2022-02-26 16:57:11 -0500

    authentication is completly done

--- pages/authPage.js
+++ pages/authPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import {auth} from '../firebase/firebase'
-import {GoogleAuthProvider, onAuthStateChanged, signInWithPopup} from 'firebase/auth'
+import { auth } from '../firebase/firebase'
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signInWithRedirect } from 'firebase/auth'
 import { useRouter } from 'next/router'
 
 const authPage = () => {
@@ -11,16 +11,16 @@ const authPage = () => {
   const [userInfo, setUserInfo] = useState(null)
 
   onAuthStateChanged(auth, (user) => {
-    if(user){
+    if (user) {
       setUserInfo(user.uid)
-    }else{
+    } else {
       setUserInfo(null)
     }
   })
 
   useEffect(() => {
     console.log(userInfo)
-    if(userInfo !== null){
+    if (userInfo !== null) {
       router.push('/home')
     }
   }, [userInfo])
@@ -30,9 +30,20 @@ const authPage = () => {
   }
 
   return (
-    <button onClick={() => {login()}}>
-      click me
-    </button>
+    <div className='flex flex-col bg-blue-500 py-10 px-10 h-screen items-center justify-center space-y-5'>
+      <h1 className='text-white font-bold text-4xl'>Scuffed Reddit</h1>
+      {/* <button type='button'  onClick={() => {console.log('hello world')}}>create post</button> */}
+
+      {/* <button onClick={login()}> */}
+        <button className='flex bg-white space-x-4 items-center px-3 rounded-xl' onClick={() => {login()}}>
+          <img src='https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip' width={30} />
+          <div>Login With Google</div>
+        </button>
+
+      {/* </button> */}
+
+
+    </div>
   )
 }
 
